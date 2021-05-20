b86c433d64b3b21a50fa65bc780728e00add238c
Aashish Chachan <63456605+aashishchachan@users.noreply.github.com>
2021-05-20 14:06:26 +0530

    ready-to-code

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const server = http.createServer((req, res)=>{
+    console.log("Server Created!");
+    const path= req.url;
+    res.end('hello form the server!');
+});
+
+server.listen('3000', '127.0.0.1', (req, res)=>{
+    console.log("Listening 3000");
+})
+
+// server.on('\', (req, res)=>{
+//     res.end('Hello from the server!')
+// })
